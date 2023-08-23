d5f71434a915590512fd33b3378922edd628cc60
Irina Yugay <105415518+YugayIrina@users.noreply.github.com>
2023-08-23 11:14:25 +0300

    Answers1 by Flask
    Signed-off-by: Irina Yugay <105415518+YugayIrina@users.noreply.github.com>

--- routs.py
+++ routs.py
@@ -2,6 +2,7 @@ from flask import Flask
 from views import AdView
 
 app = Flask("app") # what is a flask?
+# Answer: Flask is a micro web framework written in Python for creating websites and applications.
 
 app.add_url_rule("/advertisements/<int:id_ad>/", view_func=AdView.as_view('advertisements_delete'),
                  methods=['DELETE', 'GET'])
